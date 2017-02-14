061ae1bf33df6965028bac47c6b16c538a07748e
SunDevDroid <sundevdroid@gmail.com>
2017-02-15 00:36:37 +0530

    Update app.js

--- scripts/app.js
+++ scripts/app.js
@@ -368,7 +368,7 @@
   // TODO add service worker code here
   if ('serviceWorker' in navigator) {
     navigator.serviceWorker
-             .register('/service-worker.js')
+             .register('https://sundevdroid.github.io/mywebapp3/service-worker.js')
              .then(function() { console.log('Service Worker Registered'); });
   }
 })();
